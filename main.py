691db4bced5055ce2dd67032531f4231114471b1
YohanAbeysinghe <abeysingheyohan@gmail.com>
2024-01-19 23:56:57 +0530

    DImensions comment

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class SelfAttention(nn.Module):
         self.embed_size = embed_size
         self.head_dim = embed_size // heads
 
-        assert (self.head_dim * heads == embed_size), 
+        assert (self.head_dim * heads == embed_size), "embed size needs to be div by heads"
 
         self.values = nn.Linear(self.head_dim, self.head_dim, bias=False)
         self.keys = nn.Linear(self.head_dim, self.head_dim, bias=False)
@@ -16,21 +16,30 @@ class SelfAttention(nn.Module):
         self.fc_out = nn.Linear(heads*self.head_dim, embed_size)
 
     def forward(self, values, keys, query, mask):
-        N = query.shape[0]
+
+        N = query.shape[0]  # number of training examples sent.
+
         value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
 
+        # Split embedding into self.heads pieces.
         values = values.reshape(N, value_len, self.heads, self.head_dim)
         keys = keys.reshape(N, key_len, self.heads, self.head_dim)
         queries = query.reshape(N, query_len, self.heads, self.head_dim)
 
-        energy = torch.enisum("nqhd, nkhd->nhqk", [queries, keys])
+        energy = torch.enisum("nqhd, nkhd->nhqk", [queries, keys]) #Multiplication of Keys with Queries.
+        # quderies shape: (N, query_len, heads, head_dim)
+        # keys shape: (N, key_len, heads, head_dim)
+        # energy shape: (N, heads, query_len, key_len)
 
         if mask is not None:
-            energy = energy.masked_fill(mask == 0, float("-le20"))
+            energy = energy.masked_fill(mask == 0, float("-le20")) #If mask is 0, then energy is -ve infinity.
 
         attention = torch.softmax(energy / (self.embed_size ** (1/2)), dim=3)
 
         out = torch.enisum("nhql, nlhd->nqhd", [attention, values]).reshape(N, query_len, self.heads*self.head_dim)
+        # attention shape: (N, heads, query_len, key_len)
+        # values shape: (N, value_len, heads, head_dim)
+        # out shape: (N, query_len, heads, head_dim)
 
         out = self.fc_out()
 
