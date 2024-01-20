369c3b27252acc41865b6b1d4ce2cece61100a06
YohanAbeysinghe <abeysingheyohan@gmail.com>
2024-01-20 14:18:52 +0530

    Test run

--- main.py
+++ main.py
@@ -170,7 +170,7 @@ class Decoder(nn.Module):
             x = layer(x, enc_out, enc_out, trg_mask)
 
         out = self.fc_out(x)
-
+        return out
 
 
 class Trasformer(nn.Module):
@@ -190,17 +190,61 @@ class Trasformer(nn.Module):
         super(Trasformer, self).__init__()
 
         self.encoder = Encoder(
-            trg_pad_idx,
+            src_vocab_size,
+            embed_size,
+            num_layers,
+            heads, 
+            device, forward_expansion,
+            dropout,
+            max_length,
+        )
+
+        self.decoder = Decoder(
+            trg_vocab_size,
             embed_size,
             num_layers,
             heads,
-            device,
             forward_expansion,
             dropout,
+            device,
             max_length,
-        ):
+        )
 
-        
+        self.src_pad_idx = src_pad_idx
+        self.trg_pad_idx = trg_pad_idx
+        self.device = device
+
+    def make_src_mask(self, src):
+        src_mask = (src != self.src_pad_idx).unsqueeze(1).unsqueeze(2)
+        return src_mask.to(self.device)
+    
+    def make_trg_mask(self, trg):
+        N, trg_len = trg.shape
+        trg_mask = torch.tril(torch.ones((trg_len, trg_len))).expand(N, 1, trg_len, trg_len)
+        return trg_mask.to(self.device)
+    
+    def forward(self, src, trg):
+        src_mask = self.make_src_masc(src)
+        trg_mask = self.make_trg_mask(trg)
+        enc_src = self.encoder(src, src_mask)
+        out = self.decoder(trg, enc_src, src_mask, trg_mask)
+        return out
+
+
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    x = torch.tensor([[1, 5, 6, 4, 3, 9, 5, 2, 0], [1, 8, 7, 3, 4, 5, 6, 7, 2]]).to(device)
+    trg = torch.tensor([[1, 7, 4, 3, 5, 9, 2, 0], [1, 5, 6, 2, 4, 7, 6, 2]]).to(device)
+
+    src_pad_idx = 0
+    trg_pad_idx = 0
+    src_vocab_size = 10
+    trg_vocab_size = 10
+    model = Trasformer(src_vocab_size, trg_vocab_size, src_pad_idx, trg_pad_idx, device=device).to(device)
+    out = model(x, trg[:, :-1])         #target is shifted by one.
+    print(out.shape)
+
+
         
         
     
