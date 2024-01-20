abe744a13e18a068c57bf0da4d13d765e8804e87
YohanAbeysinghe <abeysingheyohan@gmail.com>
2024-01-20 11:05:51 +0530

    TrasformerBlock

--- main.py
+++ main.py
@@ -42,6 +42,31 @@ class SelfAttention(nn.Module):
         # out shape: (N, query_len, heads, head_dim)
 
         out = self.fc_out()
+        return out
+
+class TrasformerBlock(nn.Module):
+    def __init__(self, embed_size, heads, dropout, forward_expansion):
+        super(TrasformerBlock, self).__init__()
+        self.attention = SelfAttention(embed_size, heads)
+        self.norm1 = nn.LayerNorm(embed_size)       #BatchNorm - Average accross the batch, LayerNorm - Average across all examples.
+        self.norm2 = nn.LayerNorm(embed_size)
+
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embed_size, forward_expansion*embed_size), 
+            nn.Relu(),
+            nn.Linear(forward_expansion,*embed_size, embed_size)
+        )
+
+        self.droupout = nn.Dropout(dropout)
+
+
+    def forward(self, value, key, query, mask):
+        attention = self.attention(value, key, query, mask)
+        x = self.droupout(self.norm1(attention + query))
+        forward = self.feed_forward(x)
+        out = self.dropout(self.norm2(forward + x))
+        return out
+        
 
 
 
